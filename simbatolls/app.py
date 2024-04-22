f22bf1360319ccf9ae95b5cb1548a09a7d4d373b
BardiaAlavi <bardia_alavi@hotmail.com>
2024-04-22 21:02:44 +1000

    table fixx

--- simbatolls/app.py
+++ simbatolls/app.py
@@ -57,6 +57,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'validate'
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
