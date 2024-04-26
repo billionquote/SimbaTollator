997d9d8ff6ac065a7aa15a2c6d9acb962b9d79af
BardiaAlavi <bardia_alavi@hotmail.com>
2024-04-26 20:53:40 +1000

    yy

--- simbatolls/app.py
+++ simbatolls/app.py
@@ -12,7 +12,7 @@ import traceback2
 from sqlalchemy.orm import Session
 from sqlalchemy import select, column, create_engine, Table, MetaData
 from io import StringIO
-from cleaner import cleaner
+from simbatolls.cleaner import cleaner
 #from flask import current_app as app
 
 
@@ -63,7 +63,9 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'validate'
 
+#run vaccum cleaner to clean the database 
 cleaner()
+
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
