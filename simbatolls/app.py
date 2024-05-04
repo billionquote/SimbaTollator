bd3a93eb335a3fea528f72637528e7a8b9e3b255
BardiaAlavi <bardia_alavi@hotmail.com>
2024-05-04 19:27:11 +1000

    dsadsa

--- simbatolls/app.py
+++ simbatolls/app.py
@@ -808,7 +808,12 @@ def dashboard_data():
     start_date = request.args.get('start_date', (datetime.now() - timedelta(days=90)).strftime('%Y-%m-%d'))
     end_date = request.args.get('end_date', datetime.now().strftime('%Y-%m-%d'))
     tolls_chart_json = fetch_tolls_data(start_date, end_date)
-    return tolls_chart_json  # Ensure this returns JSON formatted for Plotly
+    admin_fees_chart_json = fetch_admin_fees_data(start_date, end_date)
+    #return tolls_chart_json  # Ensure this returns JSON formatted for Plotly
+    return jsonify({
+        "tolls": tolls_chart_json,
+        "adminFees": admin_fees_chart_json
+    })
 
 
 
@@ -849,9 +854,50 @@ def fetch_tolls_data(start_date, end_date):
             yaxis_title='Toll Usage Count',
             plot_bgcolor='white'
         )
+        #return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        return fig.to_dict()
+    finally:
+        session.close()
+    
+def fetch_admin_fees_data(start_date, end_date):
+    session = Session(bind=db.engine)
+    try:
+        sql = """
+        SELECT
+            EXTRACT(MONTH FROM CAST(dropoff_date_time AS DATE)) AS month,
+            EXTRACT(YEAR FROM CAST(dropoff_date_time AS DATE)) AS year,
+            SUM(admin_fee) AS total_admin_fee
+        FROM
+            summary
+        WHERE
+            CAST(dropoff_date_time AS DATE) BETWEEN :start_date AND :end_date
+        GROUP BY
+            EXTRACT(MONTH FROM CAST(dropoff_date_time AS DATE)),
+            EXTRACT(YEAR FROM CAST(dropoff_date_time AS DATE))
+        ORDER BY
+            year, month;
+        """
+        result = session.execute(text(sql), {'start_date': start_date, 'end_date': end_date}).fetchall()
+        months = [f"{row.year}-{int(row.month):02d}" for row in result]
+        fees = [row.total_admin_fee for row in result]
+
+        fig = go.Figure(data=[go.Bar(
+            x=months,
+            y=fees,
+            marker_color='#007bff',
+            text=fees,
+            textposition='auto'
+        )])
+        fig.update_layout(
+            title='Admin Fee Usage',
+            xaxis_title='Month',
+            yaxis_title='Admin Fee Total',
+            plot_bgcolor='white'
+        )
         return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     finally:
         session.close()
+
 
 
 
