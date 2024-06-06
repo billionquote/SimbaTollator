406da02b77c4373dc35f5eb73632fe7ab5293471
BardiaAlavi <bardia_alavi@hotmail.com>
2024-06-06 21:57:05 +1000

    sda

--- simbatolls/cleaner.py
+++ simbatolls/cleaner.py
@@ -7,7 +7,7 @@ def cleaner():
     database_url = os.getenv('DATABASE_URL', 'your_hardcoded_database_url')
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
-    #database_url='postgresql://jvkhatepulwmsq:4db6729008abc739d7bfdeefd19c6a6459e38f9b7dbd1b3bda2e95de5eb3d01c@ec2-54-83-138-228.compute-1.amazonaws.com:5432/d33ktsaohkqdr'
+    #database_url='postgresql://u8o7lasmharbq1:p671fb6b9ee7752b360f06d7b5cdc0c781427b938d1e3601862a2aeb6a3ea9b2f@cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d99nb7lr00tna7'
     engine = create_engine(database_url)
     Session = sessionmaker(bind=engine)
 
@@ -117,7 +117,7 @@ def summary_cleaner():
     database_url = os.getenv('DATABASE_URL', 'your_hardcoded_database_url')
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
-    #database_url='postgresql://jvkhatepulwmsq:4db6729008abc739d7bfdeefd19c6a6459e38f9b7dbd1b3bda2e95de5eb3d01c@ec2-54-83-138-228.compute-1.amazonaws.com:5432/d33ktsaohkqdr'
+    #database_url='postgresql://u8o7lasmharbq1:p671fb6b9ee7752b360f06d7b5cdc0c781427b938d1e3601862a2aeb6a3ea9b2f@cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d99nb7lr00tna7'
     engine = create_engine(database_url)
     Session = sessionmaker(bind=engine)
 
