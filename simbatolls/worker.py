55f329f3f4d6331fc82b7271afbd1d6de47cb5b4
BardiaAlavi <bardia_alavi@hotmail.com>
2024-06-06 20:52:42 +1000

    back to normal

--- simbatolls/worker.py
+++ simbatolls/worker.py
@@ -1,6 +1,5 @@
 import os
 import redis
-from urllib.parse import urlparse
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
@@ -9,8 +8,7 @@ redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 #conn = redis.from_url(redis_url, ssl_cert_reqs=None)
 conn = redis.from_url(redis_url)
-url = urlparse(os.environ.get("REDIS_URL"))
-conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
+
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
