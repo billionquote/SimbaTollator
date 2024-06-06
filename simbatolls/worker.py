c9cd4acefeb1bc6c31053d303761697393f691da
BardiaAlavi <bardia_alavi@hotmail.com>
2024-06-06 20:39:19 +1000

    ds

--- simbatolls/worker.py
+++ simbatolls/worker.py
@@ -5,13 +5,13 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-#redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 
 #conn = redis.from_url(redis_url, ssl_cert_reqs=None)
-#r = redis.from_url(os.environ.get("REDIS_URL"))
+conn = redis.from_url(redis_url)
 url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
+conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=True, ssl_cert_reqs=None)
 if __name__ == '__main__':
-    with Connection(r):
+    with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
