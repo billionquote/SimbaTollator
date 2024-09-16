57182b6bcffaed99d4bd24c4af38aff725a61139
billionquote <shahrakesh45@gmail.com>
2024-09-17 03:10:32 +0530

    Update cleaner.py
    revert cast

--- simbatolls/cleaner.py
+++ simbatolls/cleaner.py
@@ -26,85 +26,13 @@ def cleaner():
             #print('I HAVE NOW DELETED EVERYTHING')
             # Ensuring start_date is treated as a timestamp
             
-#             session.execute(text("""
-#                 CREATE TEMPORARY TABLE temp_rawdata AS 
-#                 SELECT 
-#                     distinct ON( "# Days",
-#                     start_date,
-#                     details,
-#                     CAST(lpn_tag_number AS NUMERIC),
-#                     vehicle_class,
-#                     trip_cost,
-#                     fleet_id,
-#                     end_date,
-#                     date,
-#                     rego,
-#                     res,
-#                     ref,
-#                     update,
-#                     notes,
-#                     dropoff,
-#                     day,
-#                     pickup,
-#                     pickup_date,
-#                     time_c13,
-#                     category,
-#                     vehicle,
-#                     colour,
-#                     items,
-#                     insurance,
-#                     departure,
-#                     next_rental,
-#                     pickup_date_time,
-#                     rcm_rego,
-#                     adminfeeamt)  "# Days",
-#                     start_date,
-#                     details,
-#                     lpn_tag_number,
-#                     vehicle_class,
-#                     trip_cost,
-#                     fleet_id,
-#                     end_date,
-#                     date,
-#                     rego,
-#                     res,
-#                     ref,
-#                     update,
-#                     notes,
-#                     status,
-#                     dropoff,
-#                     day,
-#                     dropoff_date,
-#                     time,
-#                     pickup,
-#                     pickup_date,
-#                     time_c13,
-#                     category,
-#                     vehicle,
-#                     colour,
-#                     items,
-#                     insurance,
-#                     departure,
-#                     next_rental,
-#                     pickup_date_time,
-#                     dropoff_date_time,
-#                     rcm_rego,
-#                     adminfeeamt
-#                 FROM rawData
-# ;
-#             """))
-
-            # Define the SQL query
-            query = text("""
+            session.execute(text("""
                 CREATE TEMPORARY TABLE temp_rawdata AS 
-                SELECT DISTINCT ON (
-                    "# Days",
+                SELECT 
+                    distinct ON( "# Days",
                     start_date,
                     details,
-                    CASE 
-                        WHEN lpn_tag_number LIKE '%\.0' THEN TRIM(TRAILING '.0' FROM lpn_tag_number)
-                        ELSE lpn_tag_number
-                    END,
+                    lpn_tag_number,
                     vehicle_class,
                     trip_cost,
                     fleet_id,
@@ -129,49 +57,121 @@ def cleaner():
                     next_rental,
                     pickup_date_time,
                     rcm_rego,
+                    adminfeeamt)  "# Days",
+                    start_date,
+                    details,
+                    lpn_tag_number,
+                    vehicle_class,
+                    trip_cost,
+                    fleet_id,
+                    end_date,
+                    date,
+                    rego,
+                    res,
+                    ref,
+                    update,
+                    notes,
+                    status,
+                    dropoff,
+                    day,
+                    dropoff_date,
+                    time,
+                    pickup,
+                    pickup_date,
+                    time_c13,
+                    category,
+                    vehicle,
+                    colour,
+                    items,
+                    insurance,
+                    departure,
+                    next_rental,
+                    pickup_date_time,
+                    dropoff_date_time,
+                    rcm_rego,
                     adminfeeamt
-                )
-                "# Days",
-                start_date,
-                details,
-                CASE 
-                    WHEN lpn_tag_number LIKE '%\.0' THEN TRIM(TRAILING '.0' FROM lpn_tag_number)
-                    ELSE lpn_tag_number
-                END AS lpn_tag_number,
-                vehicle_class,
-                trip_cost,
-                fleet_id,
-                end_date,
-                date,
-                rego,
-                res,
-                ref,
-                update,
-                notes,
-                status,
-                dropoff,
-                day,
-                dropoff_date,
-                time,
-                pickup,
-                pickup_date,
-                time_c13,
-                category,
-                vehicle,
-                colour,
-                items,
-                insurance,
-                departure,
-                next_rental,
-                pickup_date_time,
-                dropoff_date_time,
-                rcm_rego,
-                adminfeeamt
-                FROM rawData;
-            """)
+                FROM rawData
+;
+            """))
 
-            # Execute the query
-            session.execute(query)
+            # Define the SQL query
+            # query = text("""
+            #     CREATE TEMPORARY TABLE temp_rawdata AS 
+            #     SELECT DISTINCT ON (
+            #         "# Days",
+            #         start_date,
+            #         details,
+            #         CASE 
+            #             WHEN lpn_tag_number LIKE '%\.0' THEN TRIM(TRAILING '.0' FROM lpn_tag_number)
+            #             ELSE lpn_tag_number
+            #         END,
+            #         vehicle_class,
+            #         trip_cost,
+            #         fleet_id,
+            #         end_date,
+            #         date,
+            #         rego,
+            #         res,
+            #         ref,
+            #         update,
+            #         notes,
+            #         dropoff,
+            #         day,
+            #         pickup,
+            #         pickup_date,
+            #         time_c13,
+            #         category,
+            #         vehicle,
+            #         colour,
+            #         items,
+            #         insurance,
+            #         departure,
+            #         next_rental,
+            #         pickup_date_time,
+            #         rcm_rego,
+            #         adminfeeamt
+            #     )
+            #     "# Days",
+            #     start_date,
+            #     details,
+            #     CASE 
+            #         WHEN lpn_tag_number LIKE '%\.0' THEN TRIM(TRAILING '.0' FROM lpn_tag_number)
+            #         ELSE lpn_tag_number
+            #     END AS lpn_tag_number,
+            #     vehicle_class,
+            #     trip_cost,
+            #     fleet_id,
+            #     end_date,
+            #     date,
+            #     rego,
+            #     res,
+            #     ref,
+            #     update,
+            #     notes,
+            #     status,
+            #     dropoff,
+            #     day,
+            #     dropoff_date,
+            #     time,
+            #     pickup,
+            #     pickup_date,
+            #     time_c13,
+            #     category,
+            #     vehicle,
+            #     colour,
+            #     items,
+            #     insurance,
+            #     departure,
+            #     next_rental,
+            #     pickup_date_time,
+            #     dropoff_date_time,
+            #     rcm_rego,
+            #     adminfeeamt
+            #     FROM rawData;
+            # """)
+
+            # # Execute the query
+            # session.execute(query)
 
             session.execute(text("DELETE FROM rawdata;"))
 
