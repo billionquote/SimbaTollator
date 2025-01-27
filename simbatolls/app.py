8622dbbb0b58ceb8fe0cdf52851828f0e07e7697
billionquote <shahrakesh45@gmail.com>
2025-01-27 11:36:08 +0530

    Update app.py
    Vehicle API date changed to fetched vehicles 
    Line 1324

--- simbatolls/app.py
+++ simbatolls/app.py
@@ -1320,7 +1320,7 @@ async def mainRCM_df(location,fromDt,fromTime,todt,toTime,adminfeeamt):
 
     # Second API
 
-    url2 = f"{urlpath}?key={secure_key}&method=vehicle&vid=0&sd=20200101-0000&ed=20241231-0000"
+    url2 = f"{urlpath}?key={secure_key}&method=vehicle&vid=0&sd=20200101-0000&ed=20251231-0000"
     # add other parameters here as required
     shared_secret = "flEuz6agE7uKhdau3ohpxPqPeDmrFGh7"
     
