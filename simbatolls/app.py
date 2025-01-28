7250fcf6680a31db319d68c2ac0efbbf9c179a6b
billionquote <shahrakesh45@gmail.com>
2025-01-28 22:44:55 +0530

    Update app.py
    revert cast on 664 line

--- simbatolls/app.py
+++ simbatolls/app.py
@@ -659,20 +659,20 @@ def confirm_upload_task(rcm_data_json, tolls_data_json):
   
     result_tag = ps.sqldf(query_tag, locals())
 
-    # query_rego = """
-    #    SELECT DISTINCT * 
-    #    FROM tolls_df
-    #    INNER JOIN rcm_df 
-    #    ON CAST(tolls_df.[LPN/Tag number] as VARCHAR) = CAST(rcm_df.[RCM_Rego] as VARCHAR)
-    #    WHERE tolls_df.[Start Date] BETWEEN rcm_df.[Pickup Date Time] AND rcm_df.[Dropoff Date Time]
-    # """
     query_rego = """
        SELECT DISTINCT * 
        FROM tolls_df
        INNER JOIN rcm_df 
-       ON tolls_df.[LPN/Tag number] = rcm_df.[RCM_Rego]
+       ON CAST(tolls_df.[LPN/Tag number] as VARCHAR) = CAST(rcm_df.[RCM_Rego] as VARCHAR)
        WHERE tolls_df.[Start Date] BETWEEN rcm_df.[Pickup Date Time] AND rcm_df.[Dropoff Date Time]
     """
+    # query_rego = """
+    #    SELECT DISTINCT * 
+    #    FROM tolls_df
+    #    INNER JOIN rcm_df 
+    #    ON tolls_df.[LPN/Tag number] = rcm_df.[RCM_Rego]
+    #    WHERE tolls_df.[Start Date] BETWEEN rcm_df.[Pickup Date Time] AND rcm_df.[Dropoff Date Time]
+    # """
     result_rego = ps.sqldf(query_rego, locals())
     print(f'result tag I AM RESULT TAG: {result_tag.head(5)}') 
     print(f'result Rego_____: {result_rego.head(5)}') 
